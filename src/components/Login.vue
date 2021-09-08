7a0cc761c4b24598f8bdbd286f283f1a158787e8
Casper <casp8249@edu.easj.dk>
2021-09-08 13:26:46 +0200

    Remove VueBasics

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'OurNav',
+  props: {
+    msg: String
+  }
+}
+</script>
