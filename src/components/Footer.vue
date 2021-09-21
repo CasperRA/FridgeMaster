5983fc79eea7d4d8a5e759735c3466e0baaa0f55
Casper <casp8249@edu.easj.dk>
2021-09-21 11:22:33 +0200

    Commented

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,16 @@
 <template>
   <footer>
+    <!-- Our footer buttons, that will be used for navigating important pages -->
     <button id="footerButtonLeft" class="footerButton">
-      <img src="@/components/photos/user.svg" alt="" />
+      <img src="@/components/photos/user.svg" alt="User Icon" />
     </button>
 
     <button id="footerButtonCenter" class="footerButton footerCenterButton" style="visibility: hidden">
-      <img src="@/components/photos/cam.svg" alt="" />
+      <img src="@/components/photos/cam.svg" alt="Scan Icon" />
     </button>
 
     <button id="footerButtonRight" class="footerButton" @click="goBack()">
-      <img src="@/components/photos/back.svg" alt="" />
+      <img src="@/components/photos/back.svg" alt="Go Back Icon" />
     </button>
   </footer>
 </template>
