d6c752b1f4242c7e6675f564ac4bbbd62357f867
Casper <casp8249@edu.easj.dk>
2021-09-28 11:32:25 +0200

    Bulma working

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,7 @@
 </template>
 
 <style>
-body {
-     background: #5cd48c;
-}
-#app {
-  @import url(https://fonts.googleapis.com/css2?family=Comfortaa&display=swap);
-  font-family: Comfortaa, Arial;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+@import '../css/styles.css';
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -3,9 +3,11 @@
   <h1>Upload eller tag et billede</h1>
   <form id="uploadForm">
     <label id="labelUpload" for="fileToUpload">
-      <div id="uploadButton">
-        <img src="@/components/photos/upload.svg" alt="" />
+      <div class="auto"></div>
+      <div id="uploadButton" class="button image is-square is-primary is-rounded is-96x96">
+        <img class="image is-square is-96x96" src="@/components/photos/upload.svg" alt="" />
       </div>
+      <div class="auto"></div>
     </label>
     <input
       accept="image/*"
@@ -49,77 +51,3 @@ export default {
 };
 </script>
 
-<style scoped>
-#uploadedImage {
-  width: auto;
-  height: auto;
-  max-width: 100%;
-  max-height: 72.5vh;
-  margin-top: 1rem;
-}
-
-h1 {
-  margin: 0;
-  padding-top: 2rem;
-  padding-right: 2rem;
-  padding-left: 2rem;
-  color: whitesmoke;
-}
-
-#uploadForm {
-  margin: 0 auto;
-  margin-top: 1rem;
-  position: relative;
-  height: 75vh;
-  width: 90%;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-}
-
-#uploadForm::after {
-  content: "";
-  background: url("photos/foodicons025.png");
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-  opacity: 0.3;
-}
-
-#uploadForm input {
-  display: none;
-}
-
-#labelUpload {
-  display: flex;
-  width: 100%;
-}
-
-#uploadButton {
-  width: 5.5rem;
-  height: 5.5rem;
-  max-width: 5.5rem;
-  max-height: 5.5rem;
-  background-color: whitesmoke;
-  border-radius: 100%;
-  margin: auto;
-  border-style: solid;
-  border-color: #394c4f;
-  padding: auto;
-}
-
-#uploadButton img {
-  margin: auto;
-  width: 75%;
-  padding: 12.5%;
-}
-
-#loading {
-  position: absolute;
-  width: 50%;
-  z-index: 2;
-  top: 25vh;
-  left: 25%;
-}
-</style>
